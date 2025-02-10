4c1692048394464abcce1aa38b4b7a36b5c67b63
anhtht <quyetthang@vnpt.vn>
2025-02-10 16:26:22 +0700

    chỉnh sửa add-edit-category.component

--- src/app/components/cms/category-list/add-edit-category/add-edit-category.component.ts
+++ src/app/components/cms/category-list/add-edit-category/add-edit-category.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Location } from '@angular/common';
+import { DatePipe, Location, formatDate } from '@angular/common';
 import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../../../../environments/environment.development';
@@ -24,44 +24,66 @@ export class AddEditCategoryComponent implements OnInit {
         this.id = params['id'];
         this.isView = this.activatedRoute.snapshot.url[0].path === 'detail'
       }
-      console.log('isView: ',this.isView,this.id)
     })    
-        
     this.addEditForm = formBuilder.group({
-      fullName: ['',Validators.required],
-      username: ['',Validators.required],
-      gender: [''],
+      name: ['',Validators.required],
+      code: ['',Validators.required],
+      description: [''],
       phoneNumber: ['',Validators.required],
       email: [''],
       address: ['',Validators.required],
       status: [''],
-      birthday: ['']
+      birthday: [''],
+      roles: [[]]
     })
   }
   isView = false;
   id:any;
   addEditForm: FormGroup;
   confirmModal?: NzModalRef;
+  choosenFile:any;
+  avatarPath:any;
 
   setParam(form:FormGroup){
     let params: { [key: string]: any } = {}; 
     for(let key in form.controls){
       let value = form.get(key)?.value;
+      if(key == 'roles'){
+        continue;
+      }
       if(typeof(value) == 'string'){
         value = value.trim();
       }
+      if(key == 'birthday'){
+        const datePipe = new DatePipe('en-US');
+        const date = new Date(value);
+        value = datePipe.transform(date, 'yyyy-MM-dd HH:mm:ss');
+      }
+
       params[key] = value;
     }
-
+    if(this.id){
+      params['id']=this.id;
+    }
     return params;
   }
   
-  doCreateUser(){
-    console.log('httpParams: ',this.setParam(this.addEditForm));
+  doCreateOrUpdateUser(){
     let param = this.setParam(this.addEditForm);
-    this.httpClient.post(environment.apiUrl+'/users/command/users',param).subscribe((e:any)=>{
-      this.router.navigate(['/cms/category']);
-      this.message.success('Thêm mới người dùng thành công')
+    const formData = new FormData();
+    if(this.choosenFile){
+      formData.append("file",this.choosenFile);
+    }
+    
+    formData.append("data",JSON.stringify(param))
+
+    this.httpClient.post(environment.apiUrl+'/users/api/users',formData).subscribe((e:any)=>{
+      this.router.navigate(['/cms/user']);
+      if(this.id){
+        this.message.success('Cập nhật người dùng thành công')
+      }else{
+        this.message.success('Thêm mới người dùng thành công')
+      }
     })
   }
 
@@ -70,16 +92,37 @@ export class AddEditCategoryComponent implements OnInit {
       nzTitle: 'Xác nhận',
       nzContent: 'Dữ liệu chưa được lưu lại, bạn có muốn hủy bỏ?',
       nzOnOk: () => {
-        this.router.navigate(['/cms/category'])
+        this.router.navigate(['/cms/user'])
       }
     });
   }
 
+  retrieveUser(){
+    this.httpClient.get(environment.apiUrl+'/users/api/users/'+this.id).subscribe((e:any)=>{
+        console.log('retrieve user: ',e);
+        for(let key in e){
+          if(key == 'avatar'){
+            this.avatarPath = e[key];
+          }
+          // if(key == 'birthday'){
+          //   this.addEditForm.get(key)?.setValue(formatDate(e[key],'dd/MM/yyyy','Asia/Ho_Chi_Minh'));
+          // }else{
+          //   this.addEditForm.get(key)?.setValue(e[key]);
+          // }
+          this.addEditForm.get(key)?.setValue(e[key]);
+        }
+    })
+  }
+
   doChangeFile(event:any){
-    console.log('file: ',event)
+    console.log('file: ',event);
+    this.choosenFile = event;
   }
 
   ngOnInit(): void {
-    
+    if(this.id){
+      this.retrieveUser();
+    }
   }
 }
+
