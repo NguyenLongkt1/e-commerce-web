12d3f7f5cce45c96cf10c89ffa4326b4c986db2a
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2025-02-11 17:04:01 +0700

    encodeURIComponent request params

--- src/app/interceptors/auth-interceptors.ts
+++ src/app/interceptors/auth-interceptors.ts
@@ -6,18 +6,32 @@ export const authInterceptor: HttpInterceptorFn = (
     req: HttpRequest<any>,
     next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
+  let modifiedReq = req;
+  // Kiểm tra nếu request có params, thực hiện encodeURIComponent
+  if (req.params.keys().length > 0) {
+    let encodedParams = req.params.keys().reduce((params, key) => {
+      const encodedKey = encodeURIComponent(key);
+      let encodedValue = '';
+      if(req.params.get(key) && typeof req.params.get(key) === 'string'){
+        encodedValue = encodeURIComponent(req.params.get(key)?.trim()!);
+      }else{
+        encodedValue = encodeURIComponent(req.params.get(key) || '');
+      }
+      return params.set(encodedKey, encodedValue);
+    }, req.params);
+
+    modifiedReq = req.clone({ params: encodedParams });
+  }
   const token = sessionStorage?.getItem('token') ?? undefined;
   if (token) {
-    const cloned = req.clone({
+    const cloned = modifiedReq.clone({
       setHeaders: {
         authorization: 'Bearer '+token,
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Content-Type': 'application/json'
       },
     });
     return next(cloned);
   } else {
     console.log('checkToken: ',token,sessionStorage)
-    return next(req);
+    return next(modifiedReq);
   }
 };
