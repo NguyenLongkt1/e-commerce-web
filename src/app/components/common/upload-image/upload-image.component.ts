4f2305995c521ee57e1161254c0ddb0ec47ae2d1
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2025-02-12 15:21:54 +0700

    update select multiple file

--- src/app/components/common/upload-image/upload-image.component.ts
+++ src/app/components/common/upload-image/upload-image.component.ts
@@ -13,13 +13,16 @@ export class UploadImageComponent implements OnInit{
   }
 
   @Output() uploadEvent = new EventEmitter();
-  @Input() 
-  filePath:any;
-  @Input()
-  isView:any;
+  @Input() filePath:any;
+  @Input() isView:any;
+  @Input() multiple:any;
+  @Input() lstFilePath:any;
+  @Input() sizePx:any;
 
   choosenFile:any;
   previewUrl:any;
+  lstPreviewUrl:any=[];
+  listChoosenFile:any=[];
 
   onChangeFile(event:any){
     if(event.target.files){
@@ -33,17 +36,52 @@ export class UploadImageComponent implements OnInit{
     }
   }
 
+  onChangeMultipleFile(event:any){
+    if(event.target.files){
+      let temp = event.target.files;
+      for(let e of temp){
+        this.listChoosenFile.push(e);
+      }
+      for(let item of event.target.files){
+        var reader = new FileReader();
+        reader.readAsDataURL(item);
+        reader.onload = (event) => {
+          this.lstPreviewUrl.push(event.target!.result);
+        }
+      }
+      this.uploadEvent.emit(this.listChoosenFile);
+    }
+  }
+
   clearImg(){
     this.choosenFile = undefined;
     this.previewUrl = undefined;
   }
 
+  clearImgByIndex(index:any){
+    this.lstPreviewUrl.splice(index, 1);
+    this.listChoosenFile.splice(index,1);
+  }
+
   ngOnInit(): void {
+    if(!this.sizePx){
+      this.sizePx = 300;
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['filePath'] && changes['filePath'].currentValue) {
       this.previewUrl = this.filePath;
+    }
+
+    if (changes['sizePx'] && changes['sizePx'].currentValue) {
+      if(!this.sizePx){
+        this.sizePx = 300;
+      }
+    }
+
+    if (changes['lstFilePath'] && changes['lstFilePath'].currentValue) {
+      this.lstPreviewUrl = this.lstFilePath;
     }
   }
 }
