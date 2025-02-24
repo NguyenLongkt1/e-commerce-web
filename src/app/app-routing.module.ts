b3b8b839589acdf7defa28c63bb69246afebe741
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2025-02-24 10:58:16 +0700

    fix redirect path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadChildren: () => import('./routing/landing-routing.module').then(m => m.LandingRoutingModule)
+    redirectTo: '/landing'
   },
   {
     path: 'landing',
--- src/app/routing/landing-routing.module.ts
+++ src/app/routing/landing-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LandingComponent } from "../components/landing/landing.component";
+import { HomePageComponent } from "../components/landing/home-page/home-page.component";
+import { ProductDetailComponent } from "../components/landing/product-detail/product-detail.component";
 
 const routes: Routes = [
     {
-      path: '',
+      path: 'landing',
       component: LandingComponent,
       children: [
-        
+        {
+          path: '',
+          component: HomePageComponent
+        },
+        {
+          path: 'home',
+          component: HomePageComponent
+        },
+        {
+          path: 'product-detail/:id',
+          component: ProductDetailComponent,
+          pathMatch: 'full'
+        }
       ]
     }
 ];
