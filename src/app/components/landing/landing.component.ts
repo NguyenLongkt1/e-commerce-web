75c29a8819dc3e41d8f7a101ee4c355e8f0328de
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2025-02-24 11:01:42 +0700

    fix redirect path

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-
+  constructor(private router:Router){
+    router.navigate(['/landing/home'])
+  }
 }
--- src/app/components/landing/landing.module.ts
+++ src/app/components/landing/landing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from '../../routing/landing-routing.module';
+// import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
