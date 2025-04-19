cc68932d5bbd58c2ab17cab30a667b8a3e5bb35b
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2025-04-19 23:18:18 +0700

    add footer page

---
+++ src/app/components/landing/footer-page/footer-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-page',
+  standalone: false,
+  
+  templateUrl: './footer-page.component.html',
+  styleUrl: './footer-page.component.scss'
+})
+export class FooterPageComponent implements OnInit{
+
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
